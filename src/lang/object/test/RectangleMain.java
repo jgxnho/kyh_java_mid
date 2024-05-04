26bd3a7a2a9f02a3f7f1291a7ebea81012d94136
gxnho <gxnho@naver.com>
2024-05-04 19:51:59 +0900

    5.4 Address

--- src/lang/object/test/RectangleMain.java
+++ src/lang/object/test/RectangleMain.java
@@ -7,8 +7,9 @@ public class RectangleMain {
         Rectangle rect2 = new Rectangle(100, 20);
         System.out.println(rect1);
         System.out.println(rect2);
-        System.out.println(rect1 == rect2);
 
-        System.out.println(rect1.equals(rect2));
+        System.out.print("동일성 : ");
+        System.out.println(rect1 == rect2);
+        System.out.println("동등성 : " + rect1.equals(rect2));
     }
 }
