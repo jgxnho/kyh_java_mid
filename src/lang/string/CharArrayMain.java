faf20670d79f5269c1acf9b542077b762caf3319
gxnho <gxnho@naver.com>
2024-05-05 14:06:14 +0900

    5.5 stringEquals

---
+++ src/lang/string/CharArrayMain.java
@@ -0,0 +1,11 @@
+package lang.string;
+
+public class CharArrayMain {
+    public static void main(String[] args) {
+        char[] charArr = new char[]{'가', '나', '다', '라', '마'};
+        System.out.println(charArr);
+
+        String str = "가나다라마";
+        System.out.println("str = " + str);
+    }
+}
